618a6ed8ce0dc84cc04033e7eafc1892bf0cd2d3
dima9917 <dima9917@gmail.com>
2018-08-24 16:36:30 -0700

    refactor to class

--- 6-budgety/final/app.js
+++ 6-budgety/final/app.js
@@ -1,33 +1,38 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
     
-    var Expense = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-        this.percentage = -1;
-    };
-    
-    
-    Expense.prototype.calcPercentage = function(totalIncome) {
-        if (totalIncome > 0) {
-            this.percentage = Math.round((this.value / totalIncome) * 100);
-        } else {
+    class Expense {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
             this.percentage = -1;
         }
-    };
+        calcPercentage(totalIncome) {
+            if (totalIncome > 0) {
+                this.percentage = Math.round((this.value / totalIncome) * 100);
+            }
+            else {
+                this.percentage = -1;
+            }
+        }
+        getPercentage() {
+            return this.percentage;
+        }
+    }
     
     
-    Expense.prototype.getPercentage = function() {
-        return this.percentage;
-    };
     
     
-    var Income = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
+    
+    
+    class Income {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        }
+    }
     
     
     var calculateTotal = function(type) {
